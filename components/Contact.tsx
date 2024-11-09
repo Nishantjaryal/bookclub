6ab3649e8bc51b73c4c715669e7554512082eca7
Nishantjaryal <nishantjaryal24@gmail.com>
2024-11-09 10:31:38 +0530

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -15,7 +15,7 @@ const Contact = () => {
         <p className="mb-8 lg:mb-10 font-light text-center text-gray-500  sm:text-xl">
           {contactHeaders.description}
         </p>
-        <form name="contact" method="POST" data-netlify="true" className="space-y-6">
+        <form  className="space-y-6">
           <div>
             <label
               htmlFor="email"
