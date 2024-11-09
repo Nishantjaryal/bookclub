4cfcce3fc351a1c74524b435893cb48f6baf87c0
Nishantjaryal <nishantjaryal24@gmail.com>
2024-11-09 13:49:20 +0530

    resolving form errors

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,40 +1,17 @@
-'use client'
-import React, { useState } from "react";
+"use client";
+import React from "react";
 import { contactHeaders } from "@/constants/constants";
 import Link from "next/link";
 
 const Contact = () => {
-  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
-  const [status, setStatus] = useState('');
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    // Send the form data to Netlify
-    const response = await fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: new URLSearchParams(formData).toString(),
-    });
-
-    if (response.ok) {
-      setStatus('Form submitted successfully!');
-    } else {
-      setStatus('Submission failed, please try again.');
-    }
-  };
-
   return (
     <section className="flex-center bg-white gap-6 border-y-2 py-20 w-full">
       <div className="w-[40%] flex-center max-lg:hidden">
-        <img className="max-lg:hidden" src="/assets/contact.webp" alt="contact us" />
+        <img
+          className="max-lg:hidden"
+          src="/assets/contact.webp"
+          alt="contact us"
+        />
       </div>
       <div className="max-md:w-[80%] py-8 lg:py-10 px-4 w-[40%] max-w-screen-md">
         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 ">
@@ -43,23 +20,12 @@ const Contact = () => {
         <p className="mb-8 lg:mb-10 font-light text-center text-gray-500 sm:text-xl">
           {contactHeaders.description}
         </p>
-        <form
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          onSubmit={handleSubmit}
-          netlify-honeypot="bot-field"
-        >
-          {/* Required for Netlify to process the form */}
-          <input type="hidden" name="form-name" value="contact" />
-          <p hidden>
-            <label>
-              Don&apos;t fill this out if you&apos;re human: <input name="bot-field" onChange={handleChange} />
-            </label>
-          </p>
-
+        <form name="contact" netlify>
           <div>
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
               Your email
             </label>
             <input
@@ -68,12 +34,14 @@ const Contact = () => {
               name="email"
               className="mb-5 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="name@example.com"
-              onChange={handleChange}
               required
             />
           </div>
           <div>
-            <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900">
+            <label
+              htmlFor="subject"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
               Subject
             </label>
             <input
@@ -82,12 +50,14 @@ const Contact = () => {
               name="subject"
               className="block p-3 w-full text-sm text-gray-900 mb-5 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500"
               placeholder="Let us know how we can help you"
-              onChange={handleChange}
               required
             />
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">
+            <label
+              htmlFor="message"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
               Your message
             </label>
             <textarea
@@ -96,7 +66,6 @@ const Contact = () => {
               rows={5}
               className="mb-5 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
               placeholder="Leave a comment..."
-              onChange={handleChange}
               required
             ></textarea>
           </div>
@@ -107,11 +76,14 @@ const Contact = () => {
             >
               Send message
             </button>
-            <Link target="_blank" className="text-primary underline-offset-4 hover:underline" href="mailto:nishantjaryal24@gmail.com">
+            <Link
+              target="_blank"
+              className="text-primary underline-offset-4 hover:underline"
+              href="mailto:nishantjaryal24@gmail.com"
+            >
               Mail Us
             </Link>
           </div>
-          <p>{status}</p>
         </form>
       </div>
     </section>
